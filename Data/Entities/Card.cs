8ec6de9215a209331e9d8d42e899875932af7768
Serhii Denshchyk <serhii_denshchyk@epam.com>
2020-06-23 19:02:04 +0300

    Delete field constructor

--- Data/Entities/Card.cs
+++ Data/Entities/Card.cs
@@ -5,7 +5,7 @@ namespace Data.Entities
 {
     public class Card : BaseEntity
     {
-        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Created { get; set; }
 
         public int ReaderId { get; set; }
         public Reader Reader { get; set; }
