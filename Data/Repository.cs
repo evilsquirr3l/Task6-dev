9c783ad6486eb05907fccc118e462764ee845943
Serhii Denshchyk <serhii_denshchyk@epam.com>
2020-06-29 22:57:28 +0300

    Add repository pattern

---
+++ Data/Repository.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Data.Entities;
+using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly DbSet<TEntity> _dbSet;
+        
+        public Repository(LibraryDbContext context)
+        {
+            _dbSet = context.Set<TEntity>();
+        }
+ 
+        public IQueryable<TEntity> FindAll()
+        {
+            return _dbSet;
+        }
+ 
+        public IQueryable<TEntity> FindByCondition(Expression<Func<TEntity, bool>> expression)
+        {
+            return _dbSet.Where(expression);
+        }
+ 
+        public async Task Create(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+ 
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+ 
+        public void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
