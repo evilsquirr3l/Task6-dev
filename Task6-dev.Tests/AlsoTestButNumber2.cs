08b87b876cda9a3cd0297c8c1a6717ce226f2344
Serhii Denshchyk <serhii_denshchyk@epam.com>
2020-07-02 11:14:09 +0300

    Divide test to separate class

---
+++ Task6-dev.Tests/AlsoTestButNumber2.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Business.Interfaces;
+using Business.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using WebApi.Controllers;
+
+namespace Task6
+{
+    [TestFixture]
+    public class AlsoTestButNumber2
+    {
+        [Test]
+        public void BooksController_GetAll_ReturnsBooksModels()
+        {
+            //Arrange
+            var mockBookService = new Mock<IBooksService>();
+            mockBookService
+                .Setup(repo => repo.GetAll())
+                .Returns(GetTestBookModels());
+            var bookController = new BooksController(mockBookService.Object);
+            
+            //Act
+            var result = bookController.GetBooks();
+            var values = result.Result as OkObjectResult;
+            
+            //Assert
+            Assert.IsInstanceOf<ActionResult<IEnumerable<BookModel>>>(result);
+            Assert.NotNull(values.Value);
+        }
+        
+        private IEnumerable<BookModel> GetTestBookModels()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){ Id = 1, Author = "Jon Snow", Title = "A song of ice and fire", Year = 1996},
+                new BookModel(){ Id = 2, Author = "John Travolta", Title = "Pulp Fiction", Year = 1994}
+            };
+        }
+    }
+}
