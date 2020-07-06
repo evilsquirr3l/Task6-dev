62f80149818d7e1435f3902ab33c9ef102831662
Serhii Denshchyk <serhii_denshchyk@epam.com>
2020-07-06 21:39:25 +0300

    Add crud tests

--- Task6-dev.Tests/BooksTests/BooksServiceTests.cs
+++ Task6-dev.Tests/BooksTests/BooksServiceTests.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Business.Models;
 using Business.Services;
+using Data;
 using Data.Entities;
 using Data.Interfaces;
 using Moq;
@@ -11,6 +13,7 @@ namespace Task6.BooksTests
 {
     public class BooksServiceTests
     {
+        //TODO: get by filter, is book returned
         [Test]
         public void BooksService_GetAll_ReturnsBookModels()
         {
@@ -18,19 +21,93 @@ namespace Task6.BooksTests
             var mockUnitOfWork = new Mock<IUnitOfWork>();
             mockUnitOfWork
                 .Setup(m => m.BookRepository.FindAll())
-                .Returns(GetTestBookEntities());
+                .Returns(GetTestBookEntities().AsQueryable);
             var bookService = new BooksService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
 
             var actual = bookService.GetAll().ToList();
-            
-            Assert.IsInstanceOf<IEnumerable<BookModel>>(actual);
-            Assert.AreEqual(expected[0].Author, actual[0].Author);
-            Assert.AreEqual(expected[0].Title, actual[0].Title);
-            Assert.AreEqual(expected[0].Year, actual[0].Year);
-            Assert.AreEqual(expected[1].Author, actual[1].Author);
-            Assert.AreEqual(expected[1].Title, actual[1].Title);
-            Assert.AreEqual(expected[1].Year, actual[1].Year);
+
+            for (int i = 0; i < actual.Count; i++)
+            {
+                Assert.AreEqual(expected[i].Id, actual[i].Id);
+                Assert.AreEqual(expected[i].Author, actual[i].Author);
+                Assert.AreEqual(expected[i].Title, actual[i].Title);
+            }
         }
+
+        [Test]
+        public async Task BooksService_GetByIdAsync_ReturnsBookModel()
+        {
+            var expected = GetTestBookModels().First();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork
+                .Setup(m => m.BookRepository.GetByIdWithDetailsAsync(It.IsAny<int>()))
+                .ReturnsAsync(GetTestBookEntities().First);
+            var bookService = new BooksService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+
+            var actual = await bookService.GetByIdAsync(1);
+            
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.Author, actual.Author);
+            Assert.AreEqual(expected.Title, actual.Title);
+        }
+
+        private List<Book> GetTestBookEntities()
+        {
+            return new List<Book>()
+            {
+                new Book() {Id = 1, Author = "Jon Snow", Title = "A song of ice and fire", Year = 1996},
+                new Book() {Id = 2, Author = "John Travolta", Title = "Pulp Fiction", Year = 1994}
+            };
+        }
+
+        [Test]
+        public async Task BooksService_AddAsync_AddsModel()
+        {
+            //Arrange
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(x => x.BookRepository.AddAsync(It.IsAny<Book>()));
+            var bookService = new BooksService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+            
+            //Act
+            await bookService.AddAsync(new BookModel(){Id = 100, Author = "@squirr3l"});
+            
+            //Assert
+            mockUnitOfWork.Verify(x => x.BookRepository.AddAsync(It.IsAny<Book>()), Times.Once);
+            mockUnitOfWork.Verify(x => x.SaveAsync(), Times.Once);
+        }
+
+        [Test]
+        public async Task BooksService_DeleteByIdAsync_DeletesBook()
+        {
+            //Arrange
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(x => x.BookRepository.DeleteByIdAsync(It.IsAny<int>()));
+            var bookService = new BooksService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+            
+            //Act
+            await bookService.DeleteByIdAsync(1);
+            
+            //Assert
+            mockUnitOfWork.Verify(x => x.BookRepository.DeleteByIdAsync(1), Times.Once);
+            mockUnitOfWork.Verify(x => x.SaveAsync(), Times.Once);
+        }
+        
+        [Test]
+        public async Task BooksService_UpdateAsync_UpdatesBook()
+        {
+            //Arrange
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(x => x.BookRepository.Update(It.IsAny<Book>()));
+            var bookService = new BooksService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+            
+            //Act
+            await bookService.UpdateAsync(new BookModel());
+            
+            //Assert
+            mockUnitOfWork.Verify(x => x.BookRepository.Update(It.IsAny<Book>()), Times.Once);
+            mockUnitOfWork.Verify(x => x.SaveAsync(), Times.Once);
+        }
+
 
         private IEnumerable<BookModel> GetTestBookModels()
         {
@@ -39,15 +116,6 @@ namespace Task6.BooksTests
                 new BookModel(){ Id = 1, Author = "Jon Snow", Title = "A song of ice and fire", Year = 1996},
                 new BookModel(){ Id = 2, Author = "John Travolta", Title = "Pulp Fiction", Year = 1994}
             };
-        }
-        
-        private IQueryable<Book> GetTestBookEntities()
-        {
-            return new List<Book>()
-            {
-                new Book(){ Id = 1, Author = "Jon Snow", Title = "A song of ice and fire", Year = 1996},
-                new Book(){ Id = 2, Author = "John Travolta", Title = "Pulp Fiction", Year = 1994}
-            }.AsQueryable();
         }
     }
 }
