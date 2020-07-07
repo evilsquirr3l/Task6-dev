e65b5d10a39656f2eb372c123a695d89f786bfc8
Ivan Zamkovyi <izamkovyi@gmail.com>
2020-07-07 16:32:06 +0300

    Update AutoMapper settings for Reader

--- Business/AutomapperProfile.cs
+++ Business/AutomapperProfile.cs
@@ -9,8 +9,22 @@ namespace Business
         public AutomapperProfile()
         {
             CreateMap<Book, BookModel>().ReverseMap();
-            CreateMap<Reader, ReaderModel>().ReverseMap();
             CreateMap<Card, CardModel>().ReverseMap();
+
+
+            CreateMap<Reader, ReaderModel>()
+                .ForMember(destination => destination.Phone,
+                    map => map.MapFrom(source => source.ReaderProfile.Phone))
+                .ForMember(destination => destination.Address,
+                    map => map.MapFrom(source => source.ReaderProfile.Address));
+            CreateMap<ReaderModel, Reader>()
+                .ForMember(destination => destination.ReaderProfile,
+                    map => map.MapFrom(
+                        source => new ReaderProfile
+                        {
+                            Phone = source.Phone,
+                            Address = source.Address
+                        }));
         }
     }
 }
