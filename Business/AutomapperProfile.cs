9d08ef738506e5565a3b06f6367024e2a0f00281
Ivan Zamkovyi <izamkovyi@gmail.com>
2020-07-24 14:21:50 +0300

    Change CardModel to int in ReaderModel

--- Business/AutomapperProfile.cs
+++ Business/AutomapperProfile.cs
@@ -9,15 +9,15 @@ namespace Business
     {
         public AutomapperProfile()
         {
-            CreateMap<Book, BookModel>().ReverseMap();
-            CreateMap<Card, CardModel>().ReverseMap();
-
             CreateMap<Reader, ReaderModel>()
+                .ForMember(p => p.CardsIds, c => c.MapFrom(card => card.Cards.Select(x => x.Id)))
                 .ForMember(destination => destination.Phone,
                     map => map.MapFrom(source => source.ReaderProfile.Phone))
                 .ForMember(destination => destination.Address,
                     map => map.MapFrom(source => source.ReaderProfile.Address));
+
             CreateMap<ReaderModel, Reader>()
+                .ForMember(p => p.Cards, c => c.MapFrom(card => card.CardsIds))
                 .ForMember(destination => destination.ReaderProfile,
                     map => map.MapFrom(
                         source => new ReaderProfile
@@ -26,6 +26,7 @@ namespace Business
                             Phone = source.Phone,
                             Address = source.Address
                         }));
+
             CreateMap<Book, BookModel>()
                 .ForMember(p => p.CardsIds, c => c.MapFrom(card => card.Cards.Select(x => x.CardId)))
                 .ReverseMap();
